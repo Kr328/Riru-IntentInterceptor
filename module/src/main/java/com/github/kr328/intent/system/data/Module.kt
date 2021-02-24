a49239749a8019d2f98da2fb567ac9cb8a35d69e
kr328 <kr328app@outlook.com>
2021-02-24 23:50:11 +0800

    Improve: accept more package separator in <meta-data>

--- module/src/main/java/com/github/kr328/intent/system/data/Module.kt
+++ module/src/main/java/com/github/kr328/intent/system/data/Module.kt
@@ -5,12 +5,14 @@ import com.github.kr328.intent.compat.appMetaData
 import com.github.kr328.intent.shared.Constants
 import com.github.kr328.intent.system.model.Module
 
+private val REGEX_PACKAGE_SPLIT = Regex("[^0-9a-zA-Z_.]+")
+
 fun Module(packageInfo: PackageInfo): Module {
     val packageName = packageInfo.packageName
     val interceptor = packageInfo.appMetaData
         .getString(Constants.MODULE_METADATA_INTERCEPTOR, "")
     val target = packageInfo.appMetaData
-        .getString(Constants.MODULE_METADATA_TARGET, "").split(":")
+        .getString(Constants.MODULE_METADATA_TARGET, "").split(REGEX_PACKAGE_SPLIT)
 
     return Module(packageName, interceptor, target)
 }
