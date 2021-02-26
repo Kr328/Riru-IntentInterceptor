1dc21a7b82fb88a68950490f1c05c3df1056ad8f
kr328 <kr328app@outlook.com>
2021-02-26 15:08:19 +0800

    Fix: load dex by DexClassLoader (Android 6.0-7.0)

--- module/src/main/cpp/dex.c
+++ module/src/main/cpp/dex.c
@@ -7,7 +7,9 @@
 #include <errno.h>
 #include <string.h>
 #include <malloc.h>
+#include <stdlib.h>
 #include <sys/stat.h>
+#include <sys/system_properties.h>
 
 #define assert_syscall(condition, message) if (condition) do {LOGE("zygote: " message ": %s", strerror(errno)); return;} while (0)
 #define assert_load(message) if (catch_exception(env)) do {LOGE("zygote: load dex: " message " failure"); return;} while (0)
@@ -15,8 +17,23 @@
 static void *dex_data;
 static size_t dex_size;
 
+static int get_platform_api_version() {
+    int version = 0;
+    char buffer[PROP_VALUE_MAX];
+
+    if (__system_property_get("ro.build.version.sdk", buffer)) {
+        version = strtol(buffer, NULL, 10);
+    }
+
+    return version;
+}
+
 void read_dex_data() {
     if (dex_data != NULL)
+        return;
+
+    // InMemoryClassLoader unavailable in android 6.0-7.0
+    if (get_platform_api_version() < 26)
         return;
 
     scope_fd int fd = open(DEX_PATH, O_RDONLY);
@@ -51,28 +68,77 @@ static int catch_exception(JNIEnv *env) {
     return result;
 }
 
-void load_and_invoke_dex(JNIEnv *env, const char *args) {
-    if (dex_size == 0)
-        return;
+static jobject load_dex_in_memory(JNIEnv *env, jobject oSystemClassLoader) {
+    if (dex_size == 0 || get_platform_api_version() < 26)
+        return NULL;
 
-    // load dex
     jobject bufferDex = (*env)->NewDirectByteBuffer(env, dex_data, dex_size);
     jclass cDexClassLoader = (*env)->FindClass(env, "dalvik/system/InMemoryDexClassLoader");
     jmethodID mDexClassLoaderInit = (*env)->GetMethodID(env, cDexClassLoader, "<init>",
                                                         "(Ljava/nio/ByteBuffer;Ljava/lang/ClassLoader;)V");
-    jobject oDexClassLoader = (*env)->NewObject(env, cDexClassLoader,
-                                                mDexClassLoaderInit,
-                                                bufferDex,
-                                                NULL);
+    jobject oClassLoader = (*env)->NewObject(env, cDexClassLoader,
+                                             mDexClassLoaderInit,
+                                             bufferDex,
+                                             oSystemClassLoader);
 
-    assert_load("new InMemoryDexClassLoader(...)");
+    if (catch_exception(env)) {
+        return NULL;
+    }
+
+    return oClassLoader;
+}
+
+static jobject load_dex_by_file(JNIEnv *env, jobject oSystemClassLoader) {
+    jclass cDexClassLoader = (*env)->FindClass(env, "dalvik/system/DexClassLoader");
+    jmethodID mDexClassLoaderInit = (*env)->GetMethodID(env, cDexClassLoader, "<init>",
+                                                        "(Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;Ljava/lang/ClassLoader;)V");
+
+    jobject oClassLoader = (*env)->NewObject(env, cDexClassLoader,
+                                             mDexClassLoaderInit,
+                                             (*env)->NewStringUTF(env, DEX_PATH),
+                                             (*env)->NewStringUTF(env, OPTIMIZED_DIRECTORY),
+                                             NULL,
+                                             oSystemClassLoader
+    );
+
+    if (catch_exception(env)) {
+        return NULL;
+    }
+
+    return oClassLoader;
+}
+
+void load_and_invoke_dex(JNIEnv *env, const char *args) {
+    if (dex_size == 0)
+        return;
+
+    // get system class loader
+    jclass cClassLoader = (*env)->FindClass(env, "java/lang/ClassLoader");
+    jmethodID mGetSystemClassLoader = (*env)->GetStaticMethodID(env, cClassLoader,
+            "getSystemClassLoader", "()Ljava/lang/ClassLoader;");
+    jobject oSystemClassLoader = (*env)->CallStaticObjectMethod(env, cClassLoader, mGetSystemClassLoader);
+
+    assert_load("ClassLoader.getSystemClassLoader(...)");
+
+    // load dex
+    jobject oDexClassLoader = load_dex_in_memory(env, oSystemClassLoader);
+
+    if (oDexClassLoader == NULL) {
+        oDexClassLoader = load_dex_by_file(env, oSystemClassLoader);
+    }
+
+    if (oDexClassLoader == NULL) {
+        LOGE("zygote: load dex failure");
+
+        return;
+    }
 
     // get loaded dex inject method
-    jmethodID mFindClass = (*env)->GetMethodID(env, cDexClassLoader, "loadClass",
-                                               "(Ljava/lang/String;Z)Ljava/lang/Class;");
+    jmethodID mFindClass = (*env)->GetMethodID(env, cClassLoader, "loadClass",
+                                               "(Ljava/lang/String;)Ljava/lang/Class;");
     jstring sInjectClassName = (*env)->NewStringUTF(env, INJECT_CLASS_NAME);
     jclass cInject = (jclass) (*env)->CallObjectMethod(env, oDexClassLoader,
-                                                       mFindClass, sInjectClassName, (jboolean) 0);
+                                                       mFindClass, sInjectClassName);
 
     assert_load("loadClass(...)");
 
--- module/src/main/cpp/dex.h
+++ module/src/main/cpp/dex.h
@@ -6,6 +6,7 @@
 #define INJECT_CLASS_NAME "com.github.kr328.intent.InjectorKt"
 #define INJECT_METHOD_NAME "main"
 #define INJECT_METHOD_SIGNATURE "([Ljava/lang/String;)V"
+#define OPTIMIZED_DIRECTORY "/data/dalvik-cache"
 
 void read_dex_data();
 void free_dex_data();
