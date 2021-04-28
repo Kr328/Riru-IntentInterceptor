4322e8ad8f186d8af3060f5443e74e03ed3c3d07
kr328 <kr328app@outlook.com>
2021-04-28 13:08:57 +0800

    Chore: bump version

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,8 +6,8 @@ allprojects {
         jcenter()
     }
 
-    val moduleVersionCode: Int by extra(1)
-    val moduleVersionName: String by extra("1.0.0")
+    val moduleVersionCode: Int by extra(101)
+    val moduleVersionName: String by extra("1.1")
 
     val moduleMinSdkVersion: Int by extra(23)
     val moduleTargetSdkVersion: Int by extra(30)
